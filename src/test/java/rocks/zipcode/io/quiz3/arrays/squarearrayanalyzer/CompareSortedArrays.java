140ae0d8dbf697a94c7146b9ecafc798146714a2
Leon Hunter <leonhunter@Leons-MacBook-Pro.local>
2019-03-07 13:21:36 -0500

    updated tests

--- src/test/java/rocks/zipcode/io/quiz3/arrays/squarearrayanalyzer/CompareSortedArrays.java
+++ src/test/java/rocks/zipcode/io/quiz3/arrays/squarearrayanalyzer/CompareSortedArrays.java
@@ -11,43 +11,43 @@ public class CompareSortedArrays {
 
     @Test
     public void test0() {
-        Integer[] array = {1};
-        Integer[] arraySquared = {1};
+        Integer[] array = {5};
+        Integer[] arraySquared = {25};
         Assert.assertTrue(SquareArrayAnalyzer.compare(array, arraySquared));
     }
 
     @Test
     public void test1() {
-        Integer[] array = {2};
-        Integer[] arraySquared = {4};
-        Assert.assertTrue(SquareArrayAnalyzer.compare(array, arraySquared));
-    }
-
-    @Test
-    public void test2() {
         Integer[] array = {3};
         Integer[] arraySquared = {9};
         Assert.assertTrue(SquareArrayAnalyzer.compare(array, arraySquared));
     }
 
     @Test
+    public void test2() {
+        Integer[] array = {6};
+        Integer[] arraySquared = {36};
+        Assert.assertTrue(SquareArrayAnalyzer.compare(array, arraySquared));
+    }
+
+    @Test
     public void test3() {
-        Integer[] array = {1,2,3};
-        Integer[] arraySquared = {1,4,9};
+        Integer[] array = {3,2,1,0};
+        Integer[] arraySquared = {9,4,1,0};
         Assert.assertTrue(SquareArrayAnalyzer.compare(array, arraySquared));
     }
 
     @Test
     public void test4() {
-        Integer[] array = {3,4,5,};
-        Integer[] arraySquared = {9,16,25};
+        Integer[] array = {10,9,8};
+        Integer[] arraySquared = {100, 81, 64};
         Assert.assertTrue(SquareArrayAnalyzer.compare(array, arraySquared));
     }
 
     @Test
     public void test5() {
-        Integer[] array = {121, 144, 19, 161, 19, 144, 19, 11};
-        Integer[] arraySquared = {121, 14641, 20736, 361, 25921, 361, 20736, 361};
+        Integer[] array = {0, 1, 0};
+        Integer[] arraySquared = {0, 1, 0};
         Assert.assertTrue(SquareArrayAnalyzer.compare(array, arraySquared));
     }
 }
