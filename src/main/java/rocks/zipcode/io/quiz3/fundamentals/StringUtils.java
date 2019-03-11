e6b3ac7c7058bf3c4ba6202e7a4d4d84a67caf2a
Nhu Nguyen <nhu313@gmail.com>
2019-03-11 12:41:12 -0400

    removed stringutils code

--- src/main/java/rocks/zipcode/io/quiz3/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/io/quiz3/fundamentals/StringUtils.java
@@ -2,7 +2,6 @@ package rocks.zipcode.io.quiz3.fundamentals;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Queue;
 
 /**
  * @author leon on 09/12/2018.
@@ -16,18 +15,11 @@ public class StringUtils {
         return null;
     }
 
-    public static String[] getAllSubStrings(String str) {
-        List<String> list = new ArrayList<>();
-        for (int i = 0; i < str.length(); i++) {
-            for (int j = i+1; j <= str.length(); j++) {
-                list.add(str.substring(i,j));
-
-            }
-        }
-        return list.toArray(new String[0]);
+    public static String[] getAllSubStrings(String string) {
+        return null;
     }
 
     public static Integer getNumberOfSubStrings(String input){
-        return getAllSubStrings(input).length;
+        return null;
     }
 }
