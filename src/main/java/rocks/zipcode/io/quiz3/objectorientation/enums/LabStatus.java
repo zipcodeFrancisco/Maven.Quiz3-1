5d65832bff0cff7f59dd96836d023cb41e1ed982
Leon Hunter <leonhunter@Leons-MacBook-Pro.local>
2019-03-23 18:31:02 -0400

    added comments to LabStatus

--- src/main/java/rocks/zipcode/io/quiz3/objectorientation/enums/LabStatus.java
+++ src/main/java/rocks/zipcode/io/quiz3/objectorientation/enums/LabStatus.java
@@ -2,6 +2,7 @@ package rocks.zipcode.io.quiz3.objectorientation.enums;
 
 /**
  * @author leon on 10/12/2018.
+ * Ensure there are enumerations for, `COMPLETED`, `INCOMPLETE`, `PENDING`
  */
 public enum LabStatus {
     ADD_ENUMERATIONS_HERE;
