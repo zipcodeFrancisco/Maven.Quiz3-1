0922549ea0107a368725fcd0e8e0291001744a1b
Leon Hunter <leonhunter@Leons-MacBook-Pro.local>
2019-03-23 17:56:57 -0400

    updateed comments of SquareArrayAnalyzer

--- src/main/java/rocks/zipcode/io/quiz3/arrays/SquareArrayAnalyzer.java
+++ src/main/java/rocks/zipcode/io/quiz3/arrays/SquareArrayAnalyzer.java
@@ -4,7 +4,12 @@ package rocks.zipcode.io.quiz3.arrays;
  * @author leon on 09/12/2018.
  */
 public class SquareArrayAnalyzer {
-    public static Boolean compare(Integer[] input, Integer[] squaredValues) {
+    /**
+     * @param inputArray an array of integers
+     * @param squaredValues an array of integers
+     * @return true if each `inputArray[i]` has a corresponding `squaredValues[j]` such that `squaredValues[j] == inputArray[i] * inputArray[i]`
+     */
+    public static Boolean compare(Integer[] inputArray, Integer[] squaredValues) {
         return null;
     }
 }
